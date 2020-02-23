fbd79f37b61028cae424ccc0dfe58ccbed200e4d
cristoferarad <cristoferarad@yahoo.com>
2020-02-23 21:55:07 +0200

    Updates

---
+++ precache-manifest.cabd167566e2b9a8a221f9bb37872361.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "680e884f7ae9a668bfe48697915781eb",
+    "url": "/Robotii/index.html"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/Robotii/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "905de8777f50ad49f11e",
+    "url": "/Robotii/static/css/main.c2e768ee.chunk.css"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/Robotii/static/js/2.a5747d58.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Robotii/static/js/2.a5747d58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "905de8777f50ad49f11e",
+    "url": "/Robotii/static/js/main.fb37c4c8.chunk.js"
+  },
+  {
+    "revision": "6c24a1e1e4a2e5af95bc",
+    "url": "/Robotii/static/js/runtime-main.34f3e91d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robotii/static/media/SEGA.12a3db8b.woff"
+  }
+]);
