901d9fd2d35781adf5ca395a862e653b082f0cb0
cristoferarad <cristoferarad@yahoo.com>
2020-03-01 10:56:46 +0200

    adaugare redux

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,42 +1,51 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../Components/CardList';
 import SearchBox from '../Components/SearchBox';
 import Scroll from '../Components/Scroll';
 import './App.css';
 
+import { setSearchField } from '../actions';
+
+const mapStateToProps = state => {
+  return{
+    searchField: state.searchField
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+   return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+  }
+}
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-       robots: [],
-       searchfield: ''
+       robots: []
     }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(users => this.setState({ robots: users}));
   } 
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value })
-    
-    }
-
   render() {
-    const { robots, searchfield } = this.state;
+    const { robots} = this.state; 
+    const { searchField , onSearchChange } = this.props;
     const filterRobots = robots.filter(robot =>{
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
       })
 
-    return !robots.length ?
+    return !robots.length ? 
     <h1>Loading</h1> :
     (
     <div className='tc'>
       <h1 className='f2'>Robotii</h1>
-      <SearchBox searchChange={this.onSearchChange}/>
+      <SearchBox searchChange={onSearchChange}/>
       <Scroll>
        <CardList robots={filterRobots}/> 
       </Scroll>
@@ -45,4 +54,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
